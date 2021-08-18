4b4aa98359b27d0ef13ddb214ae35c1fb60023d5
Danu Agastyan <agastyandanu@gmail.com>
2021-08-18 13:41:36 +0700

    18/08/21

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const app = express();
+const ejs = require('ejs');
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+let countUserOnline = 1;
+io.on('connection', socket => {
+    socket.on('join', param => {
+        console.log('User Join');
+        countUserOnline++;
+        io.emit('countUserOnline', countUserOnline);
+    })
+    socket.on('message', param => {
+        console.log('User Send Message');
+        io.emit('message', param);
+    })
+    socket.on('disconnect', param => {
+        console.log('User Leave');
+        countUserOnline--;
+        io.emit('countUserOnline', countUserOnline);
+    })
+});
+
+server.listen(3000)
